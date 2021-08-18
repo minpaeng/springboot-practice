089aa1f145b2829eaa6214f49a1836d9d907f8d2
권민정 <22alswjd@naver.com>
2021-08-19 00:48:32 +0900

    update, delete 기능 구현 및 화면 구성 완료

--- src/main/java/com/minpaeng/springboot/service/posts/PostsService.java
+++ src/main/java/com/minpaeng/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.minpaeng.springboot.service.posts;
 
 import com.minpaeng.springboot.domain.posts.Posts;
 import com.minpaeng.springboot.domain.posts.PostsRepository;
+import com.minpaeng.springboot.web.dto.PostsListResponseDto;
 import com.minpaeng.springboot.web.dto.PostsResponseDto;
 import com.minpaeng.springboot.web.dto.PostsSaveRequestDto;
 import com.minpaeng.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +29,21 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                                    .map(PostsListResponseDto::new)
+                                    .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
